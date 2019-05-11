d40541ab7882d233559381ef2d51f6bb5e4fb7d8
Ganjobar <ganjobar@gmail.com>
2019-05-11 13:27:38 +0300

    azazaz

--- connection.js
+++ connection.js
@@ -1,16 +1,3 @@
 var mysql = require('mysql');
  
-console.log('Get connection ...');
- 
-var conn = mysql.createConnection({
-    database: 'ganjobar',
-    host: "mysql.zzz.com.ua",
-    user: "questi",
-    password: "Qwerty12345"
-});
- 
-conn.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  alert("connected")
-});
+alert("HUI")
